7c4ce6d132c63dab22ac644a72ff07c5cc6bf8f8
ashirbad-byj <ashirbad.behera@byjus.com>
2022-08-16 21:56:51 +0530

    Fix user controller

--- super_market/app/controllers/api/v1/users_controller.rb
+++ super_market/app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::UsersController < ApplicationController
     def getUser
-        @authenticated_user = User.find(@authenticated_user_id)
+        @authenticated_user = User.find(params[:authenticated_user_id])
         @data_to_send = {
+            id: @authenticated_user.id,
             employee_id: @authenticated_user.employee_id,
-            name: @authenticated_user.employee_id
+            name: @authenticated_user.employee_id,
             email_id: @authenticated_user.email_id
         }
         render json: {status:"ok", data:@data_to_send}
@@ -16,19 +17,19 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def deleteUser
-        @authenticated_user = User.find(@authenticated_user_id)
+        @authenticated_user = User.find(params[:authenticated_user_id])
 
         if @authenticated_user && @authenticated_user.employee_id == params[:employee_id] && @authenticated_user.password == params[:password]
             @authenticated_user.destroy!
             render json: {status:"ok"}
         else
-            render json: {status:"error",error: {message: "invalid employee_id or password"}}
+            render json: {status:"error",error: {message: "invalid employee_id or password provided"}}
         end
     end
 
     def editUser
-        @authenticated_user = User.find(@authenticated_user_id)
-
+        @authenticated_user = User.find(params[:authenticated_user_id])
+        @update_data = {}
         if @authenticated_user && @authenticated_user.employee_id == params[:employee_id]
             params[:data].to_unsafe_h.symbolize_keys.each do |param|
                 case param[0]
@@ -41,13 +42,14 @@ class Api::V1::UsersController < ApplicationController
                     when :password
                       @update_data[:password] = param[1]
                     else
+                    end
                 end
             if @update_data 
                 @authenticated_user.update!(@update_data)
             end
             render json: {status:"ok"}
         else
-            render json: {status:"error",error: {message: "not allowed"}}
+            render json: {status:"error",error: {message: "not allowed to edit other users data"}}
         end
     end
 end
