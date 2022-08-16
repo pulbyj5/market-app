37f040d4ac6de0413618a728aea94cbf4ca9d12a
ashirbadIndia <ashirbad.behera.india@gmail.com>
2022-08-16 15:49:45 +0530

    Customer record created

--- super_market/app/models/customer.rb
+++ super_market/app/models/customer.rb
@@ -1,4 +1,13 @@
 class Customer < ApplicationRecord
     has_many :purchases
     has_many :products, through: :purchases
+
+
+    validates :name, format: { with: /\A[a-z A-Z]+\z/, message:"Enter a valid name"}, presence: true,length: { minimum: 2, maximum: 40 ,message: "name must be 2 to 40 characters long"}
+    validates :id, numericality: true, inclusion: {in: 100..999999999999,message: "id should be 3 to 12 digits long"}, presence: true
+    validates :age, numericality: true, inclusion: {in: 0..150,message: "should be in 10 to 150"}, presence: true
+    validates :email_id, format: { with: URI::MailTo::EMAIL_REGEXP, message:"Enter a valid email address"}, presence: true,length: { minimum: 3, maximum: 40 ,message: "must be 3 to 40 characters long"}
+    validates :phone_no, numericality: true, inclusion: {in: 1000000000..9999999999, message: "phone_no should be 10 digits long"}, presence: true
+    validates :sex, presence: true,inclusion: { in: %w(male female other),message: "enter valid sex {male,female or other}" } 
+    validates :address, format: { with: /\A[a-z,._ A-Z0-9]+\z/}, presence: true,length: { minimum: 10, maximum: 100 ,message: "address must be 10 to 100 characters long"}
 end
---
+++ super_market/db/schema.rb
@@ -0,0 +1,59 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_073512) do
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "email_id"
+    t.integer "phone_no"
+    t.string "sex"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "batch"
+    t.string "type"
+    t.string "brand"
+    t.float "price"
+    t.integer "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.datetime "date_and_time"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_purchases_on_customer_id"
+    t.index ["product_id"], name: "index_purchases_on_product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "employee_id"
+    t.string "name"
+    t.string "email_id"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "purchases", "customers"
+  add_foreign_key "purchases", "products"
+end
