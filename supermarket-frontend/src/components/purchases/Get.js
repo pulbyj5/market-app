fa9bab83ed76eadb81e42e40d3d8d083fb4acd2d
Pulkit Agarwal <pulkit.agarwal@byjus.com>
2022-08-18 00:19:42 +0530

    changes in purchases get file

--- supermarket-frontend/src/components/purchases/Get.js
+++ supermarket-frontend/src/components/purchases/Get.js
@@ -15,6 +15,25 @@ const getPurchases = async (setPurchases)=>{
     setPurchases(response.data.data);
 }
 
+
+const getPurchasesWithFilters = async (e,setPurchases, filters) => {
+    Object.keys(filters).forEach((key)=>{
+        if(!filters[key]){
+            delete filters[key]
+        }
+    });
+    console.log(filters);
+    let token = getToken();
+    const response = await purchases.get('/',{
+        headers:{
+            'Authorization' : `Bearer ${token}`
+        },
+        params: filters
+    });
+    console.log(response);
+    setPurchases(response.data.data);
+}
+
 const renderRecords = (purchases)=>{
 
     return purchases.map((purchase)=>{
@@ -37,6 +56,8 @@ const renderRecords = (purchases)=>{
 const Get = (props) => {
     const [purchases, setPurchases] = useState([]);
 
+    const [filters, setFilters] = useState({id:"",customer_id:"",product_id:"",quantity:"",date_time:"",status:""});
+
     let navigate = useNavigate();
 
     useEffect(()=>{
@@ -44,18 +65,61 @@ const Get = (props) => {
     },[]);
 
     return (
-        
+         
         <div>
         
         <div className="d-flex justify-content-center py-3">
             <button type="button" className="btn btn-primary" 
-            onClick={(e)=>{navigate("./",{ replace: true })}}>
+            onClick={(e)=>{navigate("/",{ replace: true })}}>
                 Add a new Purchase</button>
         </div>
-
+            <div className="row sv">
+                <div className="col">
+                <input type="text" className="form-control" placeholder="ID"
+                value = {filters.id}
+                onChange = {(e)=> setFilters({...filters,id: parseInt(e.target.value)?parseInt(e.target.value):null})}/>
+                </div>
+                <div className="col">
+                <input type="text" className="form-control" placeholder="Customer ID"
+                value = {filters.customer_id}
+                onChange = {(e)=> setFilters({...filters,customer_id: parseInt(e.target.value)?parseInt(e.target.value):null})}/>
+                </div>
+            </div>
+            <div className="row sv">
+                <div className="col">
+                <input type="text" className="form-control" placeholder="Product ID"
+                value = {filters.product_id}
+                onChange = {(e)=> setFilters({...filters, product_id: parseInt(e.target.value)?parseInt(e.target.value):null})}/>
+                </div>
+                <div className="col">
+                <input type="text" className="form-control" placeholder="Quantity"
+                value = {filters.quantity}
+                onChange = {(e)=> setFilters({...filters,quantity: e.target.value})}/>
+                </div>
+                <div> 
+                    <p></p>   
+                    <div className="col">
+                    <input type="text" className="form-control" placeholder="Date and Time"
+                    value = {filters.date_time}
+                    onChange = {(e)=> setFilters({...filters,date_time: e.target.value})}/>
+                    </div>  
+                    <p></p>
+                  <div className="col">
+                    <input type="text" className="form-control" placeholder="status"
+                    value = {filters.status}
+                    onChange = {(e)=> setFilters({...filters,status: e.target.value})}/>
+                    </div>
+                </div>
+            </div>
+            <br></br>
+            <button className="btn btn-primary sv sh"
+            onClick={(e)=>{getPurchasesWithFilters(e,setPurchases,filters)}}
+            >Filter</button>
+            <button className="btn btn-primary sv sh" onClick={()=>{setFilters({id:"",customer_id:"",product_id:"",quantity:"",date_time:"",status:""})}}>Clear</button>
+        
         <table className="table">
             <thead>
-                <tr>
+                <tr>    
                     <th scope="col">ID</th>
                     <th scope="col">Customer ID</th>
                     <th scope="col">Product ID</th>
@@ -68,7 +132,7 @@ const Get = (props) => {
             </thead>
             <tbody>
             {renderRecords(purchases)}
-                
+            {console.log(filters)}
             </tbody>
         </table>
 
