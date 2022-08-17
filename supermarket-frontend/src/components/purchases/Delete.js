835bd0a3aea7104b67307feb3c7475d311c906c9
Pulkit Agarwal <pulkit.agarwal@byjus.com>
2022-08-17 18:33:07 +0530

    added delete page in purchase

--- supermarket-frontend/src/components/purchases/Delete.js
+++ supermarket-frontend/src/components/purchases/Delete.js
@@ -1,11 +1,42 @@
-import React, { useState } from 'react'
-import { useParams } from "react-router";
-import customers from '../../apis/purchases';
+import React from 'react'
+import { useParams,useNavigate } from "react-router";
+import purchases from '../../apis/purchases';
+import { getToken } from '../../apis/token';
 
+const goBack= (navigate) => {
+        navigate("/purchases/",{ replace: true });
+}
 
+const deletePurchase = async (id,navigate)=>{
+        let token = getToken();
+        console.log(token);
+        let response = await purchases.delete("/",{
+                data: {id:parseInt(id)},
+                headers:{ 'Authorization' : `Bearer ${token}`}
+        });
+        if(response.data.status === "ok"){
+                navigate("/purchases/",{ replace: true });
+        }
+        else{
+                console.log(response.data.error.message);
+        }
+}
 const Delete = (props)=>{
+        let navigate = useNavigate();
+        let id = useParams().id
+        return (<div className="middle">
+        <h1 className='svl'>Delete Purchase</h1>
 
-        return (<div>Delete Purchases</div>)
+        <div className='svxl'>
+                <h3>Are you sure want to delete this Purchase record?</h3>
+                <button type="submit" className="btn btn-danger svxl sh"
+                onClick = {(e)=>{deletePurchase(id,navigate)}}>Yes</button>
+                <button type="button" className="btn btn-secondary me-2 svxl sh"
+                onClick = {(e)=>{goBack(navigate)}}>
+                        No
+                </button>
+        </div>
+</div>)
 }
 
 
