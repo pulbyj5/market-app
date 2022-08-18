2e056f46713a34688a9c05247d99fc71c5abfddf
Keerthanaaaaaa <110589784+Keerthanaaaaaa@users.noreply.github.com>
2022-08-18 11:06:51 +0530

    Update products_controller.rb

--- super_market/app/controllers/api/v1/products_controller.rb
+++ super_market/app/controllers/api/v1/products_controller.rb
@@ -52,14 +52,32 @@ class Api::V1::ProductsController < ApplicationController
        
     end
 
-    def editProduct
-       @prod= Product.find(params[:id])
-       if @prod
-        @prod.update!(product_params)
-        render json: {status:"ok", data: @prod}
-       else
-        render json: {error: "Unable to update the product"},status:400
-       end
+   def editProduct
+        @product = Product.find_by!(id: params[:id])
+        @update_data = {}
+        params[:data].to_unsafe_h.symbolize_keys.each do |param|
+            case param[0] 
+                when :id  
+                  @update_data[:id] = param[1]
+                when :name
+                  @update_data[:name] = param[1]
+                when :batch
+                  @update_data[:batch] = param[1]
+                when :p_type
+                  @update_data[:p_type] = param[1]
+                when :brand
+                  @update_data[:brand] = param[1]
+                when :price
+                  @update_data[:price] = param[1]
+                when :stock
+                    @update_data[:stock] = param[1]
+                else
+            end
+        end
+        if @update_data.length > 0
+            @product.update!(@update_data)
+        end
+        render json: {status:"ok"}
     end
 
     private
